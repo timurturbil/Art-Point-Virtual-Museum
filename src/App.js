775ee9c876f6fb72dab0e23a50e94f4ef2b14e61
timurturbil <timurturbil@hotmail.com>
2021-03-21 11:41:32 +0300

    taslak

--- src/App.js
+++ src/App.js
@@ -10,77 +10,6 @@ import {
 import fire from './firebase/fire';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 export default function App() {
-  const [user, setUser] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [hasAccount, setHasAccount] = useState(false);
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-
-
-
-
-  /* const selector = useSelector((state) => state.form); */
-  const clearInputs = () => {
-    setEmail("");
-    setPassword("");
-  }
-  const clearErrors = () => {
-    setEmailError("");
-    setPasswordError("");
-  }
-  const handleSignup = () => {
-    clearErrors();
-    fire
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .catch(err => {
-        switch (err.code) {
-          case "auth/email-already-in-use":
-          case "auth/invalid-email":
-            setEmailError(err.message);
-            break;
-          case "auth/weak-password":
-            setPasswordError(err.message);
-            break;
-        }
-      })
-  }
-  const handleLogin = () => {
-    clearErrors();
-    fire
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .catch(err => {
-        switch (err.code) {
-          case "auth/invalid-email":
-          case "auth/user-disabled":
-          case "auth/user-not-found":
-            setEmailError(err.message);
-            break;
-          case "auth/wrong-password":
-            setPasswordError(err.message);
-            break;
-        }
-      })
-  }
-  const handleLogout = () => {
-    fire.auth().signOut();
-  }
-  const authListener = () => {
-    fire.auth().onAuthStateChanged((user) => {
-      if (user) {
-        clearInputs();
-        setUser(user)
-      } else {
-        setUser("")
-      }
-    });
-  }
-
-  useEffect(() => {
-    authListener();
-  }, [])
   return (
     <Router>
       <Switch>
@@ -89,7 +18,7 @@ export default function App() {
         <Route path="/OpereSalon" component={OperaScren}>
         </Route>
         <Route exact path="/">
-        <InputScreen LogOut={handleLogout}/>
+        <InputScreen/>
         </Route>
       </Switch>
     </Router>
