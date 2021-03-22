d651fd2064a68e5ce0e0e3789719b83bf62186e4
ozkalai <melihozkalayy@gmail.com>
2021-03-23 00:10:45 +0300

    uploaded Bigİmage page

--- src/screens/BigImage/BigImage.js
+++ src/screens/BigImage/BigImage.js
@@ -14,22 +14,26 @@ const BigImageScreen = (props) => {
         <InputScreen/>
     </div>
     </div> */
-    const { item } = props.location.state;
-    console.log(item)
+    const { item } = props.location && props.location.state;  
     return (
-        <div>
-            <Link to="/">Geri git</Link>
-            <img className="myImage" src={item.baseimageurl} alt="" />
-            <div className="About">
-            <div>Name: {item.about.name}</div>
-            <div>alphasort: {item.about.alphasort}</div>
-            <div>birthplace: {item.about.birthplace}</div>
-            <div>culture: {item.about.culture}</div>
-            <div>deathplace: {item.about.deathplace}</div>
-            <div>gender: {item.about.gender}</div>
-            <div>role: {item.about.role}</div>
+        <div className="section">
+            <div className="container">
+                <div className="wrapper">
+                    <Link className="link" to="/">Go Back to The Mesuem</Link>
+                    <img className="myImage" src={item.baseimageurl} alt="" />
+                    <div className="about">
+                        <p className="name">{item && item.about.name}</p>
+                        <p>alphasort: {item && item.about.alphasort}</p>
+                        <p>birthplace: {item && item.about.birthplace}</p>
+                        <p>culture: {item && item.about.culture}</p>
+                        <p>deathplace: {item && item.about.deathplace}</p>
+                        <p>gender: {item && item.about.gender}</p>
+                        <p>role: {item && item.about.role}</p>
+                    </div>
+                </div>
             </div>
         </div>
+       
     );
 }
 
